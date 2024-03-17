2b7d6715b11a19608b457aba58cf85266b06dd0a
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2024-03-17 14:52:49 -0300

    fix: polish shaky ghost animation on drop

--- src/lib/components/SortableList.svelte
+++ src/lib/components/SortableList.svelte
@@ -41,25 +41,29 @@
 	function setGhostStyles(action: 'init' | 'set' | 'unset') {
 		if (action === 'init' || action === 'set') {
 			const source = targetItem ? targetItem : draggedItem;
-			if (!source) return;
+			if (!source || !draggedItem) return;
 
 			ghostRef.style.width = `${source.width}px`;
-			ghostRef.style.left = `${source.x}px`;
+			ghostRef.style.height = `${source.height}px`;
 
-			if (action === 'init') ghostRef.style.top = `${source.y}px`;
+			if (action === 'init') {
+				ghostRef.style.left = `${source.x}px`;
+				ghostRef.style.top = `${source.y}px`;
+			}
 
 			if (action === 'set') {
-				if (draggedItem?.y && targetItem?.y) {
-					const ghostTop =
-						// Check if the dragged item is above the target item.
-						draggedItem.y < source.y ? source.y + (source.height - draggedItem.height) : source.y;
-					ghostRef.style.top = `${ghostTop}px`;
-				}
-				ghostRef.style.transition =
-					`left ${transitionDuration}ms cubic-bezier(0.2, 1, 0.1, 1),` +
-					`top ${transitionDuration}ms cubic-bezier(0.2, 1, 0.1, 1),` +
-					`transform ${transitionDuration}ms cubic-bezier(0.2, 1, 0.1, 1)`;
+				ghostRef.style.left = `${ghostRef.getBoundingClientRect().x}px`;
+				ghostRef.style.top = `${ghostRef.getBoundingClientRect().y}px`;
 				ghostRef.style.transform = 'translate3d(0, 0, 0)';
+				// setTimeout will allow the values above to be properly set before setting the ones below.
+				setTimeout(() => {
+					ghostRef.style.transition =
+						`left ${transitionDuration}ms cubic-bezier(0.2, 1, 0.1, 1),` +
+						`top ${transitionDuration}ms cubic-bezier(0.2, 1, 0.1, 1),` +
+						`transform ${transitionDuration}ms cubic-bezier(0.2, 1, 0.1, 1)`;
+					ghostRef.style.left = `${source.x}px`;
+					ghostRef.style.top = `${source.y}px`;
+				}, 0);
 			}
 		} else {
 			ghostRef.style.removeProperty('transition');
@@ -137,7 +141,6 @@
 	function handlePointerMove(event: PointerEvent) {
 		if (!isDragging || !ghostRef || !itemsOrigin || draggedItem === null) return;
 
-		/* prettier-ignore */
 		ghostRef.style.transform = `translate3d(${event.clientX - ghostOrigin.x}px, ${event.clientY - ghostOrigin.y}px, 0)`;
 
 		const collidingItem = getCollidingItem(getItemData(ghostRef), itemsOrigin, swapThreshold);
