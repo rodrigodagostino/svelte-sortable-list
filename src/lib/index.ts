abbdbb111f705788d798de8830f898e6b066cbcc
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2024-07-27 13:48:08 -0300

    BREAKING: rename icon components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 export { default as SortableList } from '$lib/components/SortableList.svelte';
-export { default as IconHandle } from '$lib/components/Icon/Handle.svelte';
-export { default as IconRemove } from '$lib/components/Icon/Remove.svelte';
+export { default as IconHandle } from '$lib/components/Icons/IconHandle.svelte';
+export { default as IconRemove } from '$lib/components/Icons/IconRemove.svelte';
 export { sortItems } from '$lib/utils/index.js';
 export { type SortableListProps } from '$lib/types/index.js';
