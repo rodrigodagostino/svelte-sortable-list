37a7338d7fe45878201710d771f512f327685dcd
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2025-05-25 01:34:37 -0300

    BREAKING: unify <IconHandle> and <IconRemove> under <Icon>

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	export let name: 'handle' | 'remove';
+
+	// Thank you, Lucide :)
+	// https://lucide.dev
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	class="ssl-icon-{name}"
+	height="18"
+	viewBox="{name === 'handle' ? 4 : 0} 0 {name === 'handle' ? 16 : 24} 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	pointer-events="none"
+>
+	{#if name === 'handle'}
+		<circle cx="9" cy="12" r="1" />
+		<circle cx="9" cy="5" r="1" />
+		<circle cx="9" cy="19" r="1" />
+		<circle cx="15" cy="12" r="1" />
+		<circle cx="15" cy="5" r="1" />
+		<circle cx="15" cy="19" r="1" />
+	{:else}
+		<path d="M18 6 6 18" />
+		<path d="m6 6 12 12" />
+	{/if}
+</svg>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,8 +2,7 @@ export { default as SortableList } from '$lib/components/SortableList.svelte';
 export { default as SortableItem } from '$lib/components/SortableItem.svelte';
 export { default as Handle } from '$lib/components/Handle.svelte';
 export { default as Remove } from '$lib/components/Remove.svelte';
-export { default as IconHandle } from '$lib/components/Icons/IconHandle.svelte';
-export { default as IconRemove } from '$lib/components/Icons/IconRemove.svelte';
+export { default as Icon } from '$lib/components/Icon.svelte';
 export { removeItem, sortItems } from '$lib/utils/index.js';
 export type {
 	SortableItemData,
