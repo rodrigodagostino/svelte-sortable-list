ade5a39e9f4d0bbe656ae8974d90fa2381d3bd95
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2025-06-17 14:55:39 -0300

    refactor: shrink getIntersectionRect() parameters names

--- src/lib/utils/collision.ts
+++ src/lib/utils/collision.ts
@@ -4,11 +4,11 @@ export function areColliding(a: DOMRect | ItemData, b: DOMRect | ItemData) {
 	return a.x + a.width > b.x && a.x < b.x + b.width && a.y + a.height > b.y && a.y < b.y + b.height;
 }
 
-function getIntersectionRect(r1: DOMRect | ItemData, r2: DOMRect | ItemData) {
-	const x1 = Math.max(r1.x, r2.x);
-	const y1 = Math.max(r1.y, r2.y);
-	const x2 = Math.min(r1.x + r1.width, r2.x + r2.width);
-	const y2 = Math.min(r1.y + r1.height, r2.y + r2.height);
+function getIntersectionRect(a: DOMRect | ItemData, b: DOMRect | ItemData) {
+	const x1 = Math.max(a.x, b.x);
+	const y1 = Math.max(a.y, b.y);
+	const x2 = Math.min(a.x + a.width, b.x + b.width);
+	const y2 = Math.min(a.y + a.height, b.y + b.height);
 
 	return { x: x1, y: y1, width: x2 - x1, height: y2 - y1, area: (x2 - x1) * (y2 - y1) };
 }
