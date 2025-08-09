8652c961d253e29eb213fbf86c1e75bacece35ea
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2025-08-09 19:23:12 -0300

    chore: update Playwright config to deal with flaky tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'pnpm build && pnpm preview',
 		port: 4173,
 	},
 	testDir: 'e2e',
+	timeout: 60 * 1000,
+	expect: {
+		timeout: 10 * 1000,
+	},
+	retries: 2,
+	use: {
+		trace: 'on-first-retry',
+	},
 });
