f288e8244863f04acb8f280ce21e5fbc661dae8b
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2025-08-09 01:21:21 -0300

    docs: capitalize list item text

--- src/routes/fixtures.ts
+++ src/routes/fixtures.ts
@@ -19,7 +19,7 @@ export const defaultRootProps: SortableList.RootProps = {
 export function getDefaultItems(length: number): SortableList.ItemData[] {
 	return Array.from({ length }, (_, i) => ({
 		id: `list-item-${i + 1}`,
-		text: `List item ${i + 1}`,
+		text: `List Item ${i + 1}`,
 	}));
 }
 
@@ -28,10 +28,10 @@ export function getVaryingItems(length: number): SortableList.ItemData[] {
 		const number = i + 1;
 		const text =
 			number % 5 === 0
-				? `List item ${number} with a bit more of content that will break into more multiple lines`
+				? `List Item ${number} with a bit more of content that will break into more multiple lines`
 				: String(number / 5).split('.')[1] === '4'
-					? `List item ${number} that will break into multiple lines`
-					: `List item ${number}`;
+					? `List Item ${number} that will break into multiple lines`
+					: `List Item ${number}`;
 		return {
 			id: `list-item-${number}`,
 			text,
@@ -42,7 +42,7 @@ export function getVaryingItems(length: number): SortableList.ItemData[] {
 export function getLockedItems(length: number): SortableList.ItemData[] {
 	return Array.from({ length }, (_, i) => ({
 		id: `locked-item-${i + 1}`,
-		text: `Locked item ${i + 1}`,
+		text: `Locked Item ${i + 1}`,
 		isLocked: true,
 	}));
 }
@@ -50,7 +50,7 @@ export function getLockedItems(length: number): SortableList.ItemData[] {
 export function getDisabledItems(length: number): SortableList.ItemData[] {
 	return Array.from({ length }, (_, i) => ({
 		id: `disabled-item-${i + 1}`,
-		text: `Disabled item ${i + 1}`,
+		text: `Disabled Item ${i + 1}`,
 		isDisabled: true,
 	}));
 }
@@ -59,32 +59,32 @@ export function getInteractiveItems(): SortableList.ItemData[] {
 	return [
 		{
 			id: 'list-item-1',
-			text: 'List item 1',
+			text: 'List Item 1',
 			type: 'input',
 		},
 		{
 			id: 'list-item-2',
-			text: 'List item 2',
+			text: 'List Item 2',
 			type: 'textarea',
 		},
 		{
 			id: 'list-item-3',
-			text: 'List item 3',
+			text: 'List Item 3',
 			type: 'select',
 		},
 		{
 			id: 'list-item-4',
-			text: 'List item 4',
+			text: 'List Item 4',
 			type: 'checkbox',
 		},
 		{
 			id: 'list-item-5',
-			text: 'List item 5',
+			text: 'List Item 5',
 			type: 'button',
 		},
 		{
 			id: 'list-item-6',
-			text: 'List item 6',
+			text: 'List Item 6',
 			type: 'a',
 		},
 	];
