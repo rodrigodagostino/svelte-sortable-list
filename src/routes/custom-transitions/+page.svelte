240ef748b4a40eec46ec05fc2567860c3ad3bc55
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2025-08-11 22:45:59 -0300

    docs: rename «With custom transitions» page to «Custom transitions»

--- src/routes/custom-transitions/+page.svelte
+++ src/routes/custom-transitions/+page.svelte
@@ -35,7 +35,7 @@
 </script>
 
 <svelte:head>
-	<title>With custom transitions — Svelte Sortable List</title>
+	<title>Custom transitions — Svelte Sortable List</title>
 </svelte:head>
 
 <SortableList.Root
