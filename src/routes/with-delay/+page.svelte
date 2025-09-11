f38fb9ddea22eccbe1b53389e8ab771a7137f25e
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2025-09-10 22:51:31 -0300

    docs: add «With delay» demo page

---
+++ src/routes/with-delay/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { SortableList, removeItem, sortItems } from '$lib/index.js';
+	import { defaultRootProps, getDefaultItems } from '../fixtures.js';
+	import { rootProps } from '../stores.js';
+	import '$lib/styles.css';
+
+	let items = getDefaultItems(5);
+
+	onMount(() => {
+		$rootProps = {
+			...defaultRootProps,
+			delay: 400,
+		};
+	});
+
+	function handleDrop(e: SortableList.RootEvents['drop']) {
+		const { draggedItemIndex, isBetweenBounds, canRemoveOnDropOut } = e.detail;
+		if (!isBetweenBounds && canRemoveOnDropOut) items = removeItem(items, draggedItemIndex);
+	}
+
+	function handleDragEnd(e: SortableList.RootEvents['dragend']) {
+		const { draggedItemIndex, targetItemIndex, isCanceled } = e.detail;
+		if (!isCanceled && typeof targetItemIndex === 'number' && draggedItemIndex !== targetItemIndex)
+			items = sortItems(items, draggedItemIndex, targetItemIndex);
+	}
+</script>
+
+<svelte:head>
+	<title>With delay — Svelte Sortable List</title>
+</svelte:head>
+
+<SortableList.Root {...$rootProps} on:drop={handleDrop} on:dragend={handleDragEnd}>
+	{#each items as item, index (item.id)}
+		<SortableList.Item {...item} {index}>
+			<div class="ssl-item-content">
+				<span class="ssl-item-content__text">{item.text}</span>
+			</div>
+		</SortableList.Item>
+	{/each}
+</SortableList.Root>
