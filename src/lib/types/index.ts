8158c9f5026ebb60812bc16ad3bbf5c815257bec
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2026-04-13 01:21:08 -0300

    chore: rename collision.ts file

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,6 +1,6 @@
 export * from './accessibility.js';
 export * from './data-extraction.js';
-export * from './states.js';
 export * from './events.js';
 export * from './exposed.js';
 export * from './props.js';
+export * from './states.js';
