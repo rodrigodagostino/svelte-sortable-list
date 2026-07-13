6344de76a4435379db6095ed1b89e49f0de8827b
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2026-07-13 02:54:32 -0300

    fix: clear target only when dragging outside of list limits

--- src/lib/components/SortableList.svelte
+++ src/lib/components/SortableList.svelte
@@ -201,7 +201,7 @@ Serves as the primary container. Provides the main structure, the drag-and-drop 
 			rootState.targetItem = ref.querySelector<HTMLLIElement>(
 				`.ssl-item[data-item-id="${collidingItemRect.id}"]`
 			);
-		else if (canClearOnDragOut || (canRemoveOnDropOut && !rootState.isBetweenBounds))
+		else if ((canClearOnDragOut || canRemoveOnDropOut) && !rootState.isBetweenBounds)
 			rootState.targetItem = null;
 	}
 
